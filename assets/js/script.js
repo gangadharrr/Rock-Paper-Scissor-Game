f48df710897bb87bf38c45ef92a4ffbc1fe4f0d6
Gangadhar C <gangadharganga90@gmail.com>
2023-05-24 16:07:39 +0530

    game features

---
+++ assets/js/script.js
@@ -0,0 +1,82 @@
+values=["rock","paper","scissors"]
+var computerScore=0;
+var playerScore=0;
+document.getElementById('computer-score-result').innerHTML=computerScore;
+document.getElementById('player-score-result').innerHTML=playerScore;
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+function getComputerChoice()
+{
+  return values[getRndInteger(0, 3)];
+}
+function gameAction(playerValue)
+{
+  document.getElementById("player-result").innerHTML=playerValue.charAt(0).toUpperCase()+playerValue.slice(1);
+  document.getElementById("player-result-img").src=`assets/images/player-${playerValue}.jpg`;
+  var ComputerChoice=getComputerChoice();
+  document.getElementById("computer-result").innerHTML=ComputerChoice.charAt(0).toUpperCase()+ComputerChoice.slice(1);
+  document.getElementById("computer-result-img").src=`assets/images/computer-${ComputerChoice}.jpg`;
+ 
+  if(playerValue==ComputerChoice)
+  {
+    document.getElementById('player-title').style.color='#000';
+        document.getElementById('computer-title').style.color='#000';
+  }
+  if(playerValue!=ComputerChoice)
+  {
+    if(playerValue=='rock'&& ComputerChoice=='scissors')
+    {
+        playerScore+=1;
+        document.getElementById('player-score-result').innerHTML=playerScore;
+        document.getElementById('player-title').style.color='#0f0';
+        document.getElementById('computer-title').style.color='#f00';
+    }
+    else if(playerValue=='rock'&& ComputerChoice=='paper')
+    {
+      computerScore+=1;
+      document.getElementById('computer-score-result').innerHTML=computerScore;
+      document.getElementById('player-title').style.color='#f00';
+      document.getElementById('computer-title').style.color='#0f0';
+
+    }
+    else if(playerValue=='paper'&& ComputerChoice=='scissors')
+    {
+      computerScore+=1;
+      document.getElementById('computer-score-result').innerHTML=computerScore;
+      document.getElementById('player-title').style.color='#f00';
+      document.getElementById('computer-title').style.color='#0f0';
+    }else if(playerValue=='paper'&& ComputerChoice=='rock')
+    {
+        playerScore+=1;
+        document.getElementById('player-score-result').innerHTML=playerScore;
+        document.getElementById('player-title').style.color='#0f0';
+        document.getElementById('computer-title').style.color='#f00';
+    }else if(playerValue=='scissors'&& ComputerChoice=='paper')
+    {
+      playerScore+=1;
+      document.getElementById('player-score-result').innerHTML=playerScore;
+      document.getElementById('player-title').style.color='#0f0';
+      document.getElementById('computer-title').style.color='#f00';
+    }
+    else if(playerValue=='scissors'&& ComputerChoice=='rock')
+    {
+      computerScore+=1;
+      document.getElementById('computer-score-result').innerHTML=computerScore;
+      document.getElementById('player-title').style.color='#f00';
+      document.getElementById('computer-title').style.color='#0f0';
+    }
+  }
+  if(playerScore==5 || computerScore==5)
+  {
+    alert(`!!!!!!!!${computerScore==5?'Computer':'Player'} Wins!!!!!!!!`);
+    restartGame();
+  }
+}
+function restartGame()
+{
+   computerScore=0;
+   playerScore=0;
+   document.getElementById('computer-score-result').innerHTML=computerScore;
+  document.getElementById('player-score-result').innerHTML=playerScore;
+}
