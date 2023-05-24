82e28c9ccd7940087fa67e7dceb426c06a7f4c8b
Gangadhar C <gangadharganga90@gmail.com>
2023-05-24 19:12:53 +0530

    .Add Responsive UI

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 values=["rock","paper","scissors"]
 var computerScore=0;
 var playerScore=0;
-document.getElementById('computer-score-result').innerHTML=computerScore;
-document.getElementById('player-score-result').innerHTML=playerScore;
+document.getElementById('computer-score-result').innerHTML=`Computer : ${computerScore}`;
+document.getElementById('player-score-result').innerHTML=`Player : ${playerScore}`;
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min) ) + min;
 }
@@ -13,10 +13,10 @@ function getComputerChoice()
 function gameAction(playerValue)
 {
   document.getElementById("player-result").innerHTML=playerValue.charAt(0).toUpperCase()+playerValue.slice(1);
-  document.getElementById("player-result-img").src=`assets/images/player-${playerValue}.jpg`;
+  document.getElementById("player-result-img").src=`assets/images/player-${playerValue}.png`;
   var ComputerChoice=getComputerChoice();
   document.getElementById("computer-result").innerHTML=ComputerChoice.charAt(0).toUpperCase()+ComputerChoice.slice(1);
-  document.getElementById("computer-result-img").src=`assets/images/computer-${ComputerChoice}.jpg`;
+  document.getElementById("computer-result-img").src=`assets/images/computer-${ComputerChoice}.png`;
  
   if(playerValue==ComputerChoice)
   {
@@ -28,14 +28,14 @@ function gameAction(playerValue)
     if(playerValue=='rock'&& ComputerChoice=='scissors')
     {
         playerScore+=1;
-        document.getElementById('player-score-result').innerHTML=playerScore;
+        document.getElementById('player-score-result').innerHTML=`Player : ${playerScore}`;
         document.getElementById('player-title').style.color='#0f0';
         document.getElementById('computer-title').style.color='#f00';
     }
     else if(playerValue=='rock'&& ComputerChoice=='paper')
     {
       computerScore+=1;
-      document.getElementById('computer-score-result').innerHTML=computerScore;
+      document.getElementById('computer-score-result').innerHTML=`Computer : ${computerScore}`;
       document.getElementById('player-title').style.color='#f00';
       document.getElementById('computer-title').style.color='#0f0';
 
@@ -43,26 +43,26 @@ function gameAction(playerValue)
     else if(playerValue=='paper'&& ComputerChoice=='scissors')
     {
       computerScore+=1;
-      document.getElementById('computer-score-result').innerHTML=computerScore;
+      document.getElementById('computer-score-result').innerHTML=`Computer : ${computerScore}`;
       document.getElementById('player-title').style.color='#f00';
       document.getElementById('computer-title').style.color='#0f0';
     }else if(playerValue=='paper'&& ComputerChoice=='rock')
     {
         playerScore+=1;
-        document.getElementById('player-score-result').innerHTML=playerScore;
+        document.getElementById('player-score-result').innerHTML=`Player : ${playerScore}`;
         document.getElementById('player-title').style.color='#0f0';
         document.getElementById('computer-title').style.color='#f00';
     }else if(playerValue=='scissors'&& ComputerChoice=='paper')
     {
       playerScore+=1;
-      document.getElementById('player-score-result').innerHTML=playerScore;
+      document.getElementById('player-score-result').innerHTML=`Player : ${playerScore}`;
       document.getElementById('player-title').style.color='#0f0';
       document.getElementById('computer-title').style.color='#f00';
     }
     else if(playerValue=='scissors'&& ComputerChoice=='rock')
     {
       computerScore+=1;
-      document.getElementById('computer-score-result').innerHTML=computerScore;
+      document.getElementById('computer-score-result').innerHTML=`Computer : ${computerScore}`;
       document.getElementById('player-title').style.color='#f00';
       document.getElementById('computer-title').style.color='#0f0';
     }
@@ -77,6 +77,6 @@ function restartGame()
 {
    computerScore=0;
    playerScore=0;
-   document.getElementById('computer-score-result').innerHTML=computerScore;
-  document.getElementById('player-score-result').innerHTML=playerScore;
+   document.getElementById('computer-score-result').innerHTML=`Computer : ${computerScore}`;
+   document.getElementById('player-score-result').innerHTML=`Player : ${playerScore}`;
 }
