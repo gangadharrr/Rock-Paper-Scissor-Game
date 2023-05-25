ac781d970b01ded3c6500a3f346b63500e831867
Gangadhar C <gangadharganga90@gmail.com>
2023-05-25 15:21:44 +0530

    .Add confetti effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,20 @@ var computerScore=0;
 var playerScore=0;
 document.getElementById('computer-score-result').innerHTML=`Computer : ${computerScore}`;
 document.getElementById('player-score-result').innerHTML=`Player : ${playerScore}`;
+function playerWin()
+{
+  document.getElementById("confetti").style.display="flex";
+  document.querySelector("#confetti h1").innerHTML=`! ! ! You Win ! ! !`;
+  document.querySelector("body").style.backgroundImage=`url("assets/images/confetti-player.gif")`;
+}
+function playerLose()
+{
+  document.getElementById("confetti").style.display="flex";
+  document.querySelector("#confetti h1").innerHTML=`! ! ! You Lose ! ! !`;
+  document.querySelector("body").style.backgroundImage=`url("assets/images/confetti-computer.gif")`;
+
+}
+    
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min) ) + min;
 }
@@ -20,8 +34,8 @@ function gameAction(playerValue)
  
   if(playerValue==ComputerChoice)
   {
-    document.getElementById('player-title').style.color='#000';
-        document.getElementById('computer-title').style.color='#000';
+    document.getElementById('player-title').style.color='#fff';
+        document.getElementById('computer-title').style.color='#fff';
   }
   if(playerValue!=ComputerChoice)
   {
@@ -69,12 +83,20 @@ function gameAction(playerValue)
   }
   if(playerScore==5 || computerScore==5)
   {
-    alert(`!!!!!!!!${computerScore==5?'Computer':'Player'} Wins!!!!!!!!`);
-    restartGame();
+    // alert(`!!!!!!!!${computerScore==5?'Computer':'Player'} Wins!!!!!!!!`);
+    computerScore==5?playerLose():playerWin();
+    setTimeout(()=>{restartGame();},3000);
+    return
   }
 }
+
+
 function restartGame()
 {
+    document.getElementById("confetti").style.display="none";
+    document.querySelector("body").style.backgroundImage=`none`;
+    document.getElementById('player-title').style.color='#fff';
+    document.getElementById('computer-title').style.color='#fff';
    computerScore=0;
    playerScore=0;
    document.getElementById('computer-score-result').innerHTML=`Computer : ${computerScore}`;
